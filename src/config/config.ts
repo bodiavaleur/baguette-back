34c3ad19172854da0b8021c06efb725cffb192a2
Bogdan Mavlo <bodiavaleur@gmail.com>
2021-12-27 19:42:29 +0200

    17: Add access token & remove passport

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,5 @@ export const config = {
   port: process.env.PORT ?? 3000,
   jwtSecret: process.env.JWT_SECRET ?? "",
   mongoCloudUrl: process.env.MONGODB_CLOUD_URL ?? "",
-  passportSecret: process.env.PASSPORT_SECRET ?? "",
-  passportCookieAgeHours: process.env.PASSPORT_COOKIE_AGE_HOURS ?? 1,
+  accessTokenExpireHours: process.env.ACCESS_TOKEN_EXPIRE_HOURS ?? 1,
 };
--- src/config/token.ts
+++ src/config/token.ts
@@ -1,5 +1,3 @@
-export const ACCESS_TOKEN_EXPIRE_TIME = 60 * 60 * 2;
-
 export enum Token {
   Access = "accessToken",
   Refresh = "refreshToken",
